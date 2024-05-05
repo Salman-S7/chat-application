d044cfecf2ffb8c803b316b05d71eb9177c7e9b8
Salman-S7 <sammyzed1505@gmail.com>
2024-05-05 22:44:06 +0530

    Ready to deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from "path";
+
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authroute.js';
@@ -11,12 +13,21 @@ import {app, server} from './socket/socket.js'
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+const __dirname = path.resolve();
+
 app.use(express.json()); // this is to parse the incoming req with json payload 
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messeges', messegeRoutes);
 app.use('/api/users', userRoutes)
+
+
+app.use(express.static(path.join(__dirname, "/frontend/chat-app/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "chat-app","dist", "index.html"));
+});
 
 server.listen(PORT, ()=>{
     connectToDb();
